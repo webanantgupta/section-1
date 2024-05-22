eea8c7de9088d429553b42086cafc4b91ed4c63c
webanantgupta <anantguptastu0698@gmail.com>
2024-05-22 20:07:48 +0530

    loops and conditions

---
+++ section1/conditions.js
@@ -0,0 +1,19 @@
+num = 34;
+if(num % 2 ==0){
+    console.log("even");
+    
+    x = "hdhdhd";
+    let y = "good"; // it is in the local scope
+    const pi= 3.141559;
+    // pi = 3;
+}
+else{
+    console.log("odd");
+}
+console.log(x);
+// console.log(y);
+// every thing is global
+
+console.log(1 == "1");
+console.log(1 === "1");
+
---
+++ section1/index.js
@@ -0,0 +1,10 @@
+console.log("hello js");
+a=22;
+console.log(a);
+// node --watch (filename)
+// ctrl + c to terminate
+
+weather = "nice";
+console.log(weather);
+console.log(typeof weather);
+console.log(typeof a);
