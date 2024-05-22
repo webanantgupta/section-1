21cb290b0546b9dbdc489da8c576353f169ca045
webanantgupta <anantguptastu0698@gmail.com>
2024-05-22 20:08:43 +0530

    loops and conditions

--- section1/conditions.js
+++ section1/conditions.js
@@ -16,4 +16,12 @@ console.log(x);
 
 console.log(1 == "1");
 console.log(1 === "1");
+console.log(1 + "1");
+console.log( null + "34");
+
+
+
+//to take power of any number
+console.log(2**4);
+console.log(Math.pow(2,6));
 
---
+++ section1/loops.js
@@ -0,0 +1,16 @@
+// for loop
+// for(let i = 1; i<=10; i++){
+//     console.log(i);
+// }
+
+// for(let i=30; i<=100; i++){
+//     if(i % 7 === 0)
+//     console.log(i);
+// }
+
+// write a programme to print all numbers divisible bt 7 & 11 in range 240 & 370
+
+for(let i=240; i<=370; i++){
+    if(i % 7 === 0 && i % 11 === 0)
+        console.log(i);
+}
