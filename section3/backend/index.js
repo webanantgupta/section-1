8b0545a2492b8683cdaad73a2dc2067d66cb783c
webanantgupta <anantguptastu0698@gmail.com>
2024-06-23 14:32:57 +0530

    revision get , post

--- section3/backend/index.js
+++ section3/backend/index.js
@@ -17,7 +17,7 @@ const port = 5000;
 
 // handling cors
 app.use(cors({
-    origin: ["http://localhost:3000"]
+    origin: ["http://localhost:5000"]
 }));
 
 app.use(express.json()); // convert json to js
--- section3/backend/routers/userRouter.js
+++ section3/backend/routers/userRouter.js
@@ -4,64 +4,88 @@ const Model = require("../models/userModel");
 // initalizing router
 const router = express.Router();
 
-router.post("/add", (req, res) => {
-  console.log(req.body);
+// router.post("/add", (req, res) => {
+//   console.log(req.body);
 
-  new Model(req.body)
-    .save()
-    .then((result) => {
-      res.status(200).json(result);
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json(err);
-    });
-  // res.send("response from user");
-});
+//   new Model(req.body)
+//     .save()
+//     .then((result) => {
+//       res.status(200).json(result);
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//       res.status(500).json(err);
+//     });
+//   // res.send("response from user");
+// });
 
-router.get("/getall", (req, res) => {
-  // res.send("response from user get all");
-  Model.find()
-    .then((result) => {
-      res.status(200).json(result);
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json(err);
-    });
-});
+// router.get("/getall", (req, res) => {
+//   // res.send("response from user get all");
+//   Model.find()
+//     .then((result) => {
+//       res.status(200).json(result);
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//       res.status(500).json(err);
+//     });
+// });
 
-// how to do  update operation
-router.put("/update/:id", (req, res) => {
-  Model.findByIdAndUpdate(req.params.id, req.body, { new: true })
-    .then((result) => {
-      res.status(200).json(result);
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json(err);
-    });
-});
+// // how to do  update operation
+// router.put("/update/:id", (req, res) => {
+//   Model.findByIdAndUpdate(req.params.id, req.body, { new: true })
+//     .then((result) => {
+//       res.status(200).json(result);
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//       res.status(500).json(err);
+//     });
+// });
 
-router.get('/getbyid/:id', (req, res) => {
-  Model.findById(req.params.id)
-      .then((result) => {
-          res.status(200).json(result);
-      }).catch((err) => {
-          console.log(err);
-          res.status(500).json(err);
-      });
-});
+// router.get('/getbyid/:id', (req, res) => {
+//   Model.findById(req.params.id)
+//       .then((result) => {
+//           res.status(200).json(result);
+//       }).catch((err) => {
+//           console.log(err);
+//           res.status(500).json(err);
+//       });
+// });
 
-router.delete("/delete/:id", () => {
-  Model.findByIdAndDelete(req.params.id)
-    .then((result) => {
-      res.status(200).json(result);
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json(err);
-    });
-});
+// router.delete("/delete/:id", () => {
+//   Model.findByIdAndDelete(req.params.id)
+//     .then((result) => {
+//       res.status(200).json(result);
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//       res.status(500).json(err);
+//     });
+// });
 // exporting this route
+
+router.post("/add", (req,res) => {
+  console.log(req.body);
+  new Model(req.body).save()   //to save the data
+  .then((result) => {
+    res.status(200).json(result); 
+  }).catch((err) => {
+    console.log(err);
+    res.status(500).json(err);
+  });
+// res.send("response from add");
+});
+
+router.get("/getall", (req,res) =>{
+res.send("response from getall");
+});
+
+router.get("/update", (req,res) => {
+  res.send("response from update");
+  });
+
+  router.get("/delete", (req,res) => {
+    res.send("response from delete");
+    });
 module.exports = router;
